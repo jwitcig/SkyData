dd48687cd7fd6c49c3476601925f9b4bfc4ee2b5
Jwitcig <jwitcig@gmail.com>
2016-03-07 16:23:52 -0600

    Initial Commit

---
+++ SkyData/SkyData.h
@@ -0,0 +1,19 @@
+//
+//  SkyData.h
+//  SkyData
+//
+//  Created by Developer on 3/7/16.
+//  Copyright © 2016 JwitApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SkyData.
+FOUNDATION_EXPORT double SkyDataVersionNumber;
+
+//! Project version string for SkyData.
+FOUNDATION_EXPORT const unsigned char SkyDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkyData/PublicHeader.h>
+
+
