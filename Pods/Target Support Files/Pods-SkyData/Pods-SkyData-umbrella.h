8e5a8e82a20484483ad6b663311a1010b71cc106
Jwitcig <jwitcig@gmail.com>
2016-03-09 23:44:05 -0600

    Correcting CocoaPods dependencies.

---
+++ Pods/Target Support Files/Pods-SkyData/Pods-SkyData-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SkyDataVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SkyDataVersionString[];
+
---
+++ Pods/Target Support Files/SwiftTools/SwiftTools-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftToolsVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftToolsVersionString[];
+
